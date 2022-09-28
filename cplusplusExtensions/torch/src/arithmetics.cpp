c9686eb5b3575e385835b707174ace1964f145a0
yiqiaoc11 <30539007+yiqiaoc11@users.noreply.github.com>
2022-09-27 19:17:57 -0700

    Update arithmetics.cpp

--- cplusplusExtensions/torch/src/arithmetics.cpp
+++ cplusplusExtensions/torch/src/arithmetics.cpp
@@ -1,14 +1,31 @@
 #include<vector>
+#include <torch/extension.h>
+
 using namespace std;
 
-int16 divide(int16 dividend, int16 divisor, int16 qval) {
-  int res = 0;
-  bool isNegative = (dividend > 0) ^ (divisor > 0);
-  dd = abs(dividend);
-  ds = abs(divisor);
-  if(dd < ds)
-    return res;  
-  for(int b=15;b>=0;b--)
-    res += (1<<b)?(dd > ds << b):0;
-  return res*(-1?isNegative:1);
+class Arithmetics {
+public:
+  Arithmetics(DlQuantization::QuantizationMode quantizationScheme) {}  
+  
+  int16 Arithmetics::divide(int16 dividend, int16 divisor) {
+    int res = 0;
+    bool isNegative = (dividend > 0) ^ (divisor > 0);
+    dd = abs(dividend);
+    ds = abs(divisor);
+    if(dd < ds)
+      return res;
+    for(int16 b=15;b>=0;b--) {
+      if(dd > ds<<b) {
+        res += 1<<b;
+        dd -= ds<<b;
+      }
+    }
+    return res*-1?isNegative:res;
+};
+
+PYBIND11_MODULE(TORCH_EXTENSION_NAME, m)
+{
+    pybind11::class_<Arithmetics>(m, "Arithmetics")
+        .def(pybind11::init<DlQuantization::QuantizationMode>())
+        .def("divide", &Arithmetics::divide)
 }
