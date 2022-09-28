c6c06d1ae51607c4536c6d7875a6427813b77063
yiqiaoc11 <30539007+yiqiaoc11@users.noreply.github.com>
2022-09-27 20:26:39 -0700

    Update arithmetics.cpp

--- cplusplusExtensions/torch/src/arithmetics.cpp
+++ cplusplusExtensions/torch/src/arithmetics.cpp
@@ -5,16 +5,16 @@ using namespace std;
 
 class Arithmetics {
 public:
-  Arithmetics(DlQuantization::QuantizationMode quantizationScheme) {}  
+  Arithmetics() {}  
   
-  int16 Arithmetics::divide(int16 dividend, int16 divisor) {
+  int Arithmetics::divide(int dividend, int divisor) {
     int res = 0;
     bool isNegative = (dividend > 0) ^ (divisor > 0);
     dd = abs(dividend);
     ds = abs(divisor);
     if(dd < ds)
       return res;
-    for(int16 b=15;b>=0;b--) {
+    for(int b=15;b>=0;b--) {
       if(dd > ds<<b) {
         res += 1<<b;
         dd -= ds<<b;
