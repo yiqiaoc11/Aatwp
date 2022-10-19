015c51449f31291161962a20bbbe2175091f5bf8
yiqiaoc11 <30539007+yiqiaoc11@users.noreply.github.com>
2022-10-18 23:30:29 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -127,13 +127,11 @@ class CMakeBuild(build_ext):
 # The information here can also be placed in setup.cfg - better separation of
 # logic and declaration, and simpler if you include description/version in a file.
 setup(
-    name="cmake_example",
+    name="cmake_aatwp",
     version="0.0.1",
-    author="Dean Moldovan",
-    author_email="dean0x7d@gmail.com",
-    description="A test project using pybind11 and CMake",
+    description="A project using pybind11 and CMake",
     long_description="",
-    ext_modules=[CMakeExtension("cmake_example")],
+    ext_modules=[CMakeExtension("cmake_aatwp")],
     cmdclass={"build_ext": CMakeBuild},
     zip_safe=False,
     extras_require={"test": ["pytest>=6.0"]},
